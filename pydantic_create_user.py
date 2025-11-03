8265ae6d60bc66424bbddda9ee513a5eee2b163e
Ismail <ismailtester@yandex.ru>
2025-11-03 23:03:15 +0300

    Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+from pydantic.alias_generators import to_camel
+
+
+class UserSchema(BaseModel):
+    """
+    Модель пользователя, описывает основные поля профиля.
+    """
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+    id: str
+    email: EmailStr
+    last_name: str
+    first_name: str
+    middle_name: str
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Модель ответа на успешное создание пользователя.
+    """
+    user: UserSchema
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Модель запроса на создание пользователя POST /api/v1/users
+    """
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+    email: EmailStr
+    password: str
+    last_name: str
+    first_name: str
+    middle_name: str
+
