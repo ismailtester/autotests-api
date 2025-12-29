d6aa70bf58cbd64504e11bc3394666d66f8b7776
Ismail <ismailtester@yandex.ru>
2025-12-29 17:28:00 +0300

    рактика написания API автотестов для работы с заданиями. олучение задания

--- fixtures/exercises.py
+++ fixtures/exercises.py
@@ -13,15 +13,19 @@ class ExerciseFixture(BaseModel):
     request: CreateExercisesRequestSchema
     response: CreateExerciseResponseSchema
 
+    @property
+    def exercise_id(self):
+        return self.response.exercise.id
+
 @pytest.fixture
 def exercises_client(function_user: UserFixture) -> ExercisesClient:
     return get_exercises_client(function_user.authentication_user)
 
 @pytest.fixture
 def function_exercise(
-        exercise_client: ExercisesClient,
+        exercises_client: ExercisesClient,
         function_course: CourseFixture
 ) -> ExerciseFixture:
     request = CreateExercisesRequestSchema(courseId=function_course.response.course.id)
-    response = exercise_client.create_exercise(request)
+    response = exercises_client.create_exercise(request)
     return ExerciseFixture(request=request, response=response)
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseResponseSchema, CreateExercisesRequestSchema
+from clients.exercises.exercises_schema import CreateExerciseResponseSchema, CreateExercisesRequestSchema, Exercise, \
+    GetExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -18,3 +19,29 @@ def assert_create_exercise_response(actual: CreateExerciseResponseSchema, expect
     assert_equal(actual.exercise.description, expected.description, name="description")
     assert_equal(actual.exercise.estimated_time, expected.estimated_time, name="estimated_time")
 
+def assert_exercise(actual: Exercise, expected: Exercise):
+    """
+    Проверяет, что фактические данные пользователя соответствуют ожидаемым.
+
+    :param actual: Фактическая информация по упражнению.
+    :param expected: Ожидаемые информация по упражнению.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, name="id")
+    assert_equal(actual.title, expected.title, name="title")
+    assert_equal(actual.course_id, expected.course_id, name="course_id")
+    assert_equal(actual.max_score, expected.max_score, name="max_score")
+    assert_equal(actual.min_score, expected.min_score, name="min_score")
+    assert_equal(actual.order_index, expected.order_index, name="order_index")
+    assert_equal(actual.description, expected.description, name="description")
+    assert_equal(actual.estimated_time, expected.estimated_time, name="estimated_time")
+
+
+def assert_get_exercise_response(get_exercise_response: GetExerciseResponseSchema, create_exercise_response: CreateExerciseResponseSchema):
+    """
+    Проверяет, что данные в ответе на получение данных об упражнении, совпадают с данными в ответе на создание упражнения
+    :param get_exercise_response: Ответ на запрос о получении данных об упражнении
+    :param create_exercise_response: Ответ от API при создании упражнении
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
