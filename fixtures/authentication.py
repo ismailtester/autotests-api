edcbf7aac731672979b3e93ce0aeb10ffaa44d08
ismailtester <ismailtester@users.noreply.github.com>
2026-01-17 16:21:19 +0000

    deploy: 751358d0639d123a52dfdf0726e36d359bac2c28

--- fixtures/authentication.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from clients.authentication.authentication_client import get_authentification_client, AuthenticationClient
-
-
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentification_client()
